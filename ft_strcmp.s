5d2732a8b225ec2fb8516e60a766bb436f66cb52
Oleg Mulqr <sedric@mi-b1.msk.21-school.ru>
2021-02-10 17:49:19 +0300

    final ver

--- ft_strcmp.s
+++ ft_strcmp.s
@@ -1,19 +1,19 @@
 section    .text
 global    _ft_strcmp
 
-_ft_strcmp:                           ; dest = rdi, src = rsi
+_ft_strcmp:                             ; dest = rdi, src = rsi
             mov   rax, 0
-            mov   rbx, 0
+            mov   rdx, 0
 cmp:
-            mov   al, BYTE[rdi]       ; temp1 = dest
-            cmp   al, 0               ; temp1 == '\0' ?
-            je    return              ; yes, go return                       
-            cmp   al, BYTE[rsi]       ; temp1 == src ?
-            jne   return              ; no, go return
-            inc   rdi                 ; dest++
-            inc   rsi                 ; src++
-            jmp   cmp                 ; go again
+            mov   al, BYTE[rdi + rdx]   ; temp1 = dest
+            cmp   al, 0                 ; temp1 == '\0' ?
+            je    return                ; yes, go return                       
+            cmp   al, BYTE[rsi + rdx]   ; temp1 == src ?
+            jne   return                ; no, go return
+            inc   rdi                   ; dest++
+            inc   rsi                   ; src++
+            jmp   cmp                   ; go again
 return:
-            mov     bl, BYTE[rsi]     ; temp2 = src
-            sub		rax, rbx          ; temp1 -= temp2
-            ret                       ; return(temp1)
+            mov     dl, BYTE[rsi + rdx] ; temp2 = src
+            sub		rax, rdx            ; temp1 -= temp2
+            ret                         ; return(temp1)
